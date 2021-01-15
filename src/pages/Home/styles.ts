8c34e734423a3c69e85174cae680fb19d6fd403c
Everton Oliveira <tonn.oliveira2011@gmail.com>
2021-01-14 21:50:16 -0300

    chore: Configurado estrutura do projeto

---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Header = styled.div `
+
+`;
+
+export const ConatinerSearch = styled.div `
+
+`
+
+export const ContainerCountry = styled.div `
+
+`
